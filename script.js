a6299fa82dd4960174b9ff2355d646d784eb1826
AayanNaushadAli <aayannaushad12@gmail.com>
2025-09-29 00:12:19 +0530

    learning day 1

---
+++ script.js
@@ -0,0 +1,39 @@
+const me = {
+    fullName: "Aayan Naushad Ali",
+    age: 19,
+    occupation: "Student",
+    location: "Delhi, India"
+}
+
+console.log("My name is " + me.fullName);
+
+me["fullName"] = "Aayan Naushad";
+
+console.log("My name is " + me.fullName);
+
+// task 1 
+const product = {
+    name: "pen",
+    price: 2.7,
+    rating: 4.5,
+    off: "5%",
+};
+
+console.log(product);
+
+// task 2
+
+const profile = {
+    fullName: "Aayan Naushad Ali",
+    posts: 120,
+    followers: 350,
+    following: 180,
+    userName: "aayan_ali",
+    bio: "Learning JavaScript",
+    profession: "Studnt",
+    birthday: "2006-06-12",
+    hobbies: ["reading", "coding", "gaming"],
+    isVerified: false
+};
+
+console.log(profile);
